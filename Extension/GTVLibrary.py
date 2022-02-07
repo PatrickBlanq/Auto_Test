f68da12f0f208ef14f6c1a7cd14301785cf120b8
Patrick <nq67761448@gmail.com>
2022-02-07 19:20:19 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   .idea/inspectionProfiles/profiles_settings.xml 	new file:   .idea/vcs.xml 	modified:   Extension/GTVLibrary.py 	modified:   Extension/__pycache__/GTVLibrary.cpython-38.pyc

--- Extension/GTVLibrary.py
+++ Extension/GTVLibrary.py
@@ -1,66 +1,70 @@
 
-#Add seconds to your time
-def CalculateTimeAdd(timeOriginal,seconds):
-   result = TimeToSeconds(timeOriginal) + int(seconds)
-   result = SecondsToTime(result)
-   print("当前时间戳为:", result)
-   return result
 
-#Subtract seconds to your time
-def CalculateTimeSub(timeOriginal,seconds):
-   result = TimeToSeconds(timeOriginal) - int(seconds)
-   result = SecondsToTime(result)
-   print("当前时间戳为:", result)
-   return result
+def calculate_time_add(time_original, seconds):
+    # Add seconds to your time
+    result = time_to_seconds(time_original) + int(seconds)
+    result = seconds_to_time(result)
+    print("当前时间戳为:", result)
+    return result
 
-#Translate time to seconds
-def TimeToSeconds(timeOriginal):
-   if timeOriginal != '0':
-      timeArry= timeOriginal.strip().split(":")
 
-      #If time doesn't have hour attribute 
-      if len(timeArry) == 2:
-         timeOriginal = "0:" + timeOriginal
+def calculate_time_sub(time_original, seconds):
+    # Subtract seconds to your time
+    result = time_to_seconds(time_original) - int(seconds)
+    result = seconds_to_time(result)
+    print("当前时间戳为:", result)
+    return result
 
-      h,m,s = timeOriginal.strip().split(":")
-      result= int(h) * 3600 + int(m) * 60 + int(s)
-      return result
-   else:
-      return 0
 
-#Translate seconds to time      
-def SecondsToTime(timeOriginal):   
-   h = timeOriginal // 3600;
-   m = timeOriginal % 3600 // 60;
-   s = timeOriginal % 60;   
-   
-   if h < 10:
-      if h != 0:
-         h =  "0" + str(h)
-   if m < 10:
-      m =  "0" + str(m)
-   if s < 10:
-      s =  "0" + str(s)
-   if h == 0:
-      result= str(m) + ":" + str(s)
-   else:
-      result= str(h) +":" + str(m) + ":" + str(s)
-   print(result)
-   return result
+def time_to_seconds(time_original):
+    # Translate time to seconds
+    if time_original != '0':
+        time_array = time_original.strip().split(":")
+        # If time doesn't have hour attribute
+        if len(time_array) == 2:
+            time_original = "0:" + time_original
 
-#Wether the two strings are equal
-def TwoStringEqual(str1, str2):
-   if str1.strip  == str2.strip :
-      return True
-   return False
+        h, m, s = time_original.strip().split(":")
+        result = int(h) * 3600 + int(m) * 60 + int(s)
+        return result
+    else:
+        return 0
 
-#Return the num of the string
-def GetNumOfString(data, num):
-   if len(data) < int(num):
-      return data
-   else:
-      return data[0 : int(num)]
 
-#GetNumOfString("Aelius Maximus Decimus Meridius was a high ranking Roman general",10)
-#SecondsToTime(32501)
-#CalculateTime("1:1:20","59")
+def seconds_to_time(time_original):
+    # Translate seconds to time
+    h = time_original // 3600
+    m = time_original % 3600 // 60
+    s = time_original % 60
+    if h < 10:
+        if h != 0:
+            h = "0" + str(h)
+    if m < 10:
+        m = "0" + str(m)
+    if s < 10:
+        s = "0" + str(s)
+    if h == 0:
+        result = str(m) + ":" + str(s)
+    else:
+        result = str(h) + ":" + str(m) + ":" + str(s)
+    print(result)
+    return result
+
+
+def two_string_equal(str1, str2):
+    # Whether the two strings are equal
+    if str1.strip == str2.strip:
+        return True
+    return False
+
+
+def get_num_of_string(data, num):
+    # Return the num of the string
+    if len(data) < int(num):
+        return data
+    else:
+        return data[0: int(num)]
+
+# GetNumOfString("Aelius Maximus Decimus Meridius was a high ranking Roman general",10)
+# SecondsToTime(32501)
+# CalculateTime("1:1:20","59")
